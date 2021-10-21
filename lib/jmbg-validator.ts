9aa7bf0cf07a168e224261a882884fff755c49ed
Sibin Grasic <sibin.grasic@oblak.studio>
2021-10-21 13:51:14 +0200

    fix: Fixed code styling

--- lib/jmbg-validator.ts
+++ lib/jmbg-validator.ts
@@ -1,21 +1,24 @@
 import { mod11 } from './utils';
 
-export function validJMBG(jmbg: number|string): boolean {
-  if(typeof jmbg == 'number') {
-      jmbg = jmbg.toString();
+export function validJMBG(jmbg: number | string): boolean {
+  if (typeof jmbg == 'number') {
+    jmbg = jmbg.toString();
   }
 
-  const day  = jmbg.substring(0,2);
+  const day = jmbg.substring(0, 2);
   const mnth = jmbg.substring(2, 4);
-  const year = parseInt(jmbg.substring(4,7)) > parseInt((new Date).getFullYear().toString().substring(2)) ? `1${jmbg.substring(4,7)}` : `2${jmbg.substring(4,7)}`;
+  const year =
+    parseInt(jmbg.substring(4, 7)) > parseInt(new Date().getFullYear().toString().substring(2))
+      ? `1${jmbg.substring(4, 7)}`
+      : `2${jmbg.substring(4, 7)}`;
   const time = Date.parse(`${mnth}/${day}/${year}`);
 
   if (isNaN(time)) {
-      return false;
+    return false;
   }
 
   if (['60', '66'].includes(jmbg.substring(7, 9))) {
-      return true;
+    return true;
   }
 
   return parseInt(jmbg[12], 10) === mod11(jmbg.substring(0, 12));
--- lib/mb-validator.ts
+++ lib/mb-validator.ts
@@ -5,10 +5,10 @@ import { mod11 } from './utils';
  * @param {number|string} mb Company number (MB)
  * @returns {boolean}        True if the company number (MB) is valid, false otherwise
  */
-export function validMB(mb: number | string): boolean{
+export function validMB(mb: number | string): boolean {
   if (typeof mb === 'number') {
-      mb = mb.toString();
+    mb = mb.toString();
   }
 
-  return mb.length == 8 && parseInt(mb[7], 10) === mod11(mb.substring(0,7));
+  return mb.length == 8 && parseInt(mb[7], 10) === mod11(mb.substring(0, 7));
 }
